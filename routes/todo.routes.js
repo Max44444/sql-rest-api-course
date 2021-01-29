5a35c75f9c046ecaefda957fd2fef0c3d63d9b83
Max44444 <Skorokhodov.m.v@ukr.net>
2021-01-29 21:48:27 +0200

    reqlize push route

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,24 +1,57 @@
 const { Router } = require('express')
+const Todo = require('../models/todo')
 const router = Router()
 
 // get todo list
 router.get('/', (req, res) => {
-
+  try {
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 // create new todo
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  try {
+    const todo = await Todo.create({
+      title: req.body.title,
+      done: false
+    })
+    res.status(201).json({ todo })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 // change todo
 router.put('/:id', (req, res) => {
-
+  try {
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 // delete todo
 router.delete('/:id', (req, res) => {
-
+  try {
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 module.exports = router
