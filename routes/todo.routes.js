497d178cd38db2c7e46fc296d455e441ceca96ec
Max44444 <Skorokhodov.m.v@ukr.net>
2021-01-29 19:36:12 +0200

    routes setting

---
+++ routes/todo.routes.js
@@ -0,0 +1,24 @@
+const { Router } = require('express')
+const router = Router()
+
+// get todo list
+router.get('/', (req, res) => {
+
+})
+
+// create new todo
+router.post('/', (req, res) => {
+
+})
+
+// change todo
+router.put('/:id', (req, res) => {
+
+})
+
+// delete todo
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router
