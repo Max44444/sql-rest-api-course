40f473bc4b4ba90364bf6dc192ae328a3df9464b
Max44444 <Skorokhodov.m.v@ukr.net>
2021-01-29 20:54:03 +0200

    connect to db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const sequelize = require('./utils/database')
 const todoRoutes = require('./routes/todo.routes')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -10,4 +11,13 @@ app.use((req, res, next) => {
   res.sendFile('/index.html')
 })
 
+async function start() {
+  try {
+    await sequelize.sync()
+  } catch (error) {
+    console.log(error) 
+  }
+}
+
+start()
 app.listen(PORT);
